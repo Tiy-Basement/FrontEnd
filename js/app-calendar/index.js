a73cb51f1e7bd91efd630cf561ca8f294adb4880
Cary Duncan <duncan.cary@gmail.com>
2015-12-01 15:53:20 -0500

    preliminary calendar stuff

--- js/app-calendar/index.js
+++ js/app-calendar/index.js
@@ -1,8 +1,10 @@
 import angular from 'angular';
+import 'angular-ui-calendar';
+// import fullcalendar
 
 import HomeController from './controllers/home.controller';
 
 angular
-  .module('app.auth', [])
+  .module('app.auth', ['ui.calendar'])
   .controller('HomeController', HomeController)
 ;
