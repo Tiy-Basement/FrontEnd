f3299cd9bda7bb840ce740099c3d6fcdb3126cc2
Cary Duncan <duncan.cary@gmail.com>
2015-12-01 16:16:34 -0500

    Fixed homecontroller issue

--- js/app-calendar/index.js
+++ js/app-calendar/index.js
@@ -1,10 +1,15 @@
+// require('fullcalendar');
+// require('angular-ui-calendar');
+
 import angular from 'angular';
+import moment from 'moment';
+import $ from 'jquery';
+import 'fullcalendar';
 import 'angular-ui-calendar';
-// import fullcalendar
 
 import HomeController from './controllers/home.controller';
 
 angular
-  .module('app.auth', ['ui.calendar'])
+  .module('app.calendar', ['ui.calendar'])
   .controller('HomeController', HomeController)
 ;
