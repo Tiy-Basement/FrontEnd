8c29f1094e43127883c46ff0910b0cc956630521
John Rae <john.rae23@gmail.com>
2015-12-01 10:21:28 -0500

    ready to merge

--- app/js/main.js
+++ app/js/main.js
@@ -54,9 +54,39 @@ var _controllersLoginController = require('./controllers/login.controller');
 
 var _controllersLoginController2 = _interopRequireDefault(_controllersLoginController);
 
-_angular2['default'].module('app.auth', ['ui.router']);
+var _servicesSignupService = require('./services/signup.service');
 
-},{"./controllers/login.controller":1,"./controllers/signup.controller":2,"angular":11,"angular-ui-router":9}],4:[function(require,module,exports){
+var _servicesSignupService2 = _interopRequireDefault(_servicesSignupService);
+
+_angular2['default'].module('app.auth', ['ui.router']).controller('SignupController', _controllersSignupController2['default']).controller('LoginController', _controllersLoginController2['default']).service('SignupService', _servicesSignupService2['default']);
+
+},{"./controllers/login.controller":1,"./controllers/signup.controller":2,"./services/signup.service":4,"angular":12,"angular-ui-router":10}],4:[function(require,module,exports){
+'use strict';
+
+Object.defineProperty(exports, '__esModule', {
+  value: true
+});
+var SignupService = function SignupService($http) {
+
+  function User(userObj) {
+    this.name = userObj.name;
+    this.email = userObj.email;
+    this.phone = userObj.phone;
+    this.password = userObj.password;
+  }
+
+  function addUser(userObj) {
+    var u = new User(userObj);
+    // return $http.post(url, u, /*  server headers/config  */);
+  }
+};
+
+SignupService.$inject = ['$http'];
+
+exports['default'] = SignupService;
+module.exports = exports['default'];
+
+},{}],5:[function(require,module,exports){
 'use strict';
 
 Object.defineProperty(exports, '__esModule', {
@@ -89,7 +119,7 @@ config.$inject = ['$stateProvider', '$urlRouterProvider'];
 exports['default'] = config;
 module.exports = exports['default'];
 
-},{}],5:[function(require,module,exports){
+},{}],6:[function(require,module,exports){
 'use strict';
 
 function _interopRequireDefault(obj) { return obj && obj.__esModule ? obj : { 'default': obj }; }
@@ -106,7 +136,7 @@ var _config2 = _interopRequireDefault(_config);
 
 _angular2['default'].module('app.core', ['ui.router']).config(_config2['default']);
 
-},{"./config":4,"angular":11,"angular-ui-router":9}],6:[function(require,module,exports){
+},{"./config":5,"angular":12,"angular-ui-router":10}],7:[function(require,module,exports){
 // this is throwing an error; it needs something as a parameter
 // on line 4 and the same thing in the single quotes on line 14.
 
@@ -129,7 +159,7 @@ SplashController.$inject = [];
 exports['default'] = SplashController;
 module.exports = exports['default'];
 
-},{}],7:[function(require,module,exports){
+},{}],8:[function(require,module,exports){
 'use strict';
 
 function _interopRequireDefault(obj) { return obj && obj.__esModule ? obj : { 'default': obj }; }
@@ -144,7 +174,7 @@ var _controllersSplashController2 = _interopRequireDefault(_controllersSplashCon
 
 _angular2['default'].module('app.layout', []).controller('SplashController', _controllersSplashController2['default']);
 
-},{"./controllers/splash.controller":6,"angular":11}],8:[function(require,module,exports){
+},{"./controllers/splash.controller":7,"angular":12}],9:[function(require,module,exports){
 'use strict';
 
 function _interopRequireDefault(obj) { return obj && obj.__esModule ? obj : { 'default': obj }; }
@@ -161,7 +191,7 @@ require('./app-auth/index');
 
 _angular2['default'].module('app', ['app.core', 'app.layout', 'app.auth']);
 
-},{"./app-auth/index":3,"./app-core/index":5,"./app-layout/index":7,"angular":11}],9:[function(require,module,exports){
+},{"./app-auth/index":3,"./app-core/index":6,"./app-layout/index":8,"angular":12}],10:[function(require,module,exports){
 /**
  * State-based routing for AngularJS
  * @version v0.2.15
@@ -4532,7 +4562,7 @@ angular.module('ui.router.state')
   .filter('isState', $IsStateFilter)
   .filter('includedByState', $IncludedByStateFilter);
 })(window, window.angular);
-},{}],10:[function(require,module,exports){
+},{}],11:[function(require,module,exports){
 /**
  * @license AngularJS v1.4.8
  * (c) 2010-2015 Google, Inc. http://angularjs.org
@@ -33551,11 +33581,11 @@ $provide.value("$locale", {
 })(window, document);
 
 !window.angular.$$csp().noInlineStyle && window.angular.element(document.head).prepend('<style type="text/css">@charset "UTF-8";[ng\\:cloak],[ng-cloak],[data-ng-cloak],[x-ng-cloak],.ng-cloak,.x-ng-cloak,.ng-hide:not(.ng-hide-animate){display:none !important;}ng\\:form{display:block;}.ng-animate-shim{visibility:hidden;}.ng-anchor{position:absolute;}</style>');
-},{}],11:[function(require,module,exports){
+},{}],12:[function(require,module,exports){
 require('./angular');
 module.exports = angular;
 
-},{"./angular":10}]},{},[8])
+},{"./angular":11}]},{},[9])
 
 
 //# sourceMappingURL=main.js.map
--- js/app-auth/index.js
+++ js/app-auth/index.js
@@ -3,7 +3,11 @@ import 'angular-ui-router';
 
 import SignupController from './controllers/signup.controller';
 import LoginController from './controllers/login.controller';
+import SignupService from './services/signup.service';
 
 angular
   .module('app.auth', ['ui.router'])
+  .controller('SignupController', SignupController)
+  .controller('LoginController', LoginController)
+  .service('SignupService', SignupService)
 ;
