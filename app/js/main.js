c0be10ea2de8f6b7ea4181af27aa74304e405580
Cary Duncan <duncan.cary@gmail.com>
2015-12-02 12:33:43 -0500

    Still struggling with full calendar/ui-calendar

--- app/js/main.js
+++ app/js/main.js
@@ -150,6 +150,21 @@ var HomeController = function HomeController($scope) {
   console.log('Hey, home page!');
 
   $scope.eventSources = [];
+
+  // $scope.uiConfig = {
+  //   calendar:{
+  //     height: 450,
+  //     editable: true,
+  //     header:{
+  //       left: 'month basicWeek basicDay agendaWeek agendaDay',
+  //       center: 'title',
+  //       right: 'today prev,next'
+  //     },
+  //     dayClick: $scope.alertEventOnClick,
+  //     eventDrop: $scope.alertOnDrop,
+  //     eventResize: $scope.alertOnResize
+  //   }
+  // };
 };
 
 HomeController.$inject = ['$scope'];
