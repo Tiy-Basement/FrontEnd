fb17ea3fe2e61d86f8504c513d248a77959f571e
Cary Duncan <duncan.cary@gmail.com>
2015-12-09 11:06:54 -0500

    Rerouting from forms (add event, add group)

--- js/app-calendar/controllers/addEvent.controller.js
+++ js/app-calendar/controllers/addEvent.controller.js
@@ -1,14 +1,15 @@
-let AddEventController = function($scope, AddService) {
+let AddEventController = function($scope, AddService, $state) {
   
   $scope.addEvent = function(eventObj) {
     AddService.addEvent(eventObj).then((res) => {
       console.log(res);
+      $state.go('root.home', { id: res.data.event.user_id });
     });
   };
 
 
 };
 
-AddEventController.$inject = ['$scope', 'AddService'];
+AddEventController.$inject = ['$scope', 'AddService', '$state'];
 
 export default AddEventController;
