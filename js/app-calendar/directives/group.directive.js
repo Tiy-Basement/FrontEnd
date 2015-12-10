1958455653c7fd3d00a333cd57c58f40e8c0b36a
Cary Duncan <duncan.cary@gmail.com>
2015-12-10 11:32:33 -0500

    created group directive

---
+++ js/app-calendar/directives/group.directive.js
@@ -0,0 +1,26 @@
+let groupItem = function($state, UserService) {
+  
+  return {
+    restrict: 'E',
+    replace: true,
+    scope: {
+      group: '='
+    },
+    template: `
+      <div class="sideGroup" ng-click="vm.getGroups(group)">
+        <div>{{ group.name }}</div>
+      </div>
+    `,
+    controller: 'HomeController as vm',
+    link: function (scope, element, attrs) {
+      element.on('click', function () {
+        $state.go('root.group', {id: scope.somethingsomethingId});
+      });
+    }
+  };
+
+};
+
+groupItem.$inject = ['$state', 'UserService'];
+
+export default groupItem;
