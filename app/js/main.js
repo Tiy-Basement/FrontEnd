017a803b8f9f5d32e33387fac3bc422ac86832c6
John Rae <john.rae23@gmail.com>
2015-12-16 11:33:51 -0500

    fucked up styles

--- app/js/main.js
+++ app/js/main.js
@@ -300,7 +300,7 @@ var CalendarController = function CalendarController($scope, $compile, uiCalenda
     headers: {
       'Access-Token': tkn
     },
-    color: '#7D90C3',
+    color: '#272255',
     cache: true,
     lazyFetching: true
   };
@@ -322,8 +322,7 @@ var CalendarController = function CalendarController($scope, $compile, uiCalenda
     },
     lazyFetching: true,
     cache: true,
-    rendering: 'background',
-    backgroundColor: 'red'
+    color: '#1A1B41'
   };
 
   $scope.eventSources = [$scope.myEvents];
